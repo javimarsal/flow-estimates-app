2294d63a0ef512846c807d50ddfe7f4fccdf0617
jmaus67 <javierms5@hotmail.com>
2022-02-13 20:42:28 +0100

    Modified (refactoring): quitados metodos y variable innecesarios del servicio work-item

--- frontend/src/app/services/work-item.service.ts
+++ frontend/src/app/services/work-item.service.ts
@@ -1,33 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { WorkItem } from 'src/app/models/work-item';
-import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkItemService {
-  // TODO: los workItems se deben filtrar aquí, abrá que pasarle el nombre del tablero por el que se quiere filtrar
-  workItems: WorkItem[];
-  workItems$: Subject<WorkItem[]>;
-
-
   URL_API = 'http://localhost:4000/api/workitems';
 
-  //workItems: WorkItem[] = [];
-
-  constructor(private http: HttpClient) {
-    this.workItems = [];
-    this.workItems$ = new Subject();
-  }
-
-  sendWorkItems(workItems: WorkItem[]) {
-    this.workItems$.next(workItems);
-  }
-
-  getWorkItems$(): Observable<WorkItem[]> {
-    return this.workItems$.asObservable();
-  }
+  constructor(private http: HttpClient) { }
 
   getWorkItems() {
     return this.http.get<WorkItem[]>(this.URL_API);
