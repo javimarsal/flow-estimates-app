631196ce658f36646b27a07d7b8a9fd2c0491b6c
jmaus67 <javierms5@hotmail.com>
2022-02-14 19:58:32 +0100

    Modified (refactoring): movidos unos metodos del servicio work-item a la clase del componente

--- frontend/src/app/components/work-item/work-item.component.ts
+++ frontend/src/app/components/work-item/work-item.component.ts
@@ -70,11 +70,41 @@ export class WorkItemComponent implements OnInit {
   }
 
   filterWorkItems_ByPanelName(workItems: WorkItem[], panelName: string): WorkItem[] {
-    return this.workItemService.filterWorkItems_ByPanelName(workItems, panelName);
+    // array de WorkItem donde guardaremos los workItems, según el panelName
+    let workItemsOfPanel: WorkItem[] = [];
+
+    for(let workItem of workItems) {
+      // Nombre del Panel que le corresponde al workItem
+      let workItemPanel = workItem.panel;
+
+      // Si el panel del workItem corresponde con panelName, lo guardamos
+      if(workItemPanel == panelName) {
+        workItemsOfPanel.push(workItem);
+      }
+    }
+
+    return workItemsOfPanel;
   }
 
   getWorkItemsNames(workItems: WorkItem[]): string[] {
-    return this.workItemService.getWorkItemsNames(workItems);
+    let workItemsNames: string[] = [];
+
+    // Ordenar los workItems por el número de posición
+    let sortedWorkItems_ByPosition = this.sortWorkItems(workItems);
+
+    // Obtiene el nombre de cada workItem
+    for(let workItem of sortedWorkItems_ByPosition) {
+      workItemsNames.push(workItem.name);
+    }
+
+    return workItemsNames;
+  }
+
+  // Ordena los workItems según el número de posición
+  sortWorkItems(workItems: WorkItem[]): WorkItem[] {
+    return workItems.sort(function(a, b) {
+      return a.position - b.position;
+    });
   }
 
 
--- frontend/src/app/services/work-item.service.ts
+++ frontend/src/app/services/work-item.service.ts
@@ -18,42 +18,4 @@ export class WorkItemService {
     return this.http.put(this.URL_API + `/${workItem?._id}`, workItem);
   }
 
-  filterWorkItems_ByPanelName(workItems: WorkItem[], panelName: string): WorkItem[] {
-    // array de WorkItem donde guardaremos los workItems, según el panelName
-    let workItemsOfPanel: WorkItem[] = [];
-
-    for(let workItem of workItems) {
-      // Nombre del Panel que le corresponde al workItem
-      let workItemPanel = workItem.panel;
-
-      // Si el panel del workItem corresponde con panelName, lo guardamos
-      if(workItemPanel == panelName) {
-        workItemsOfPanel.push(workItem);
-      }
-    }
-
-    return workItemsOfPanel;
-  }
-
-  getWorkItemsNames(workItems: WorkItem[]): string[] {
-    let workItemsNames: string[] = [];
-
-    // Ordenar los workItems por el número de posición
-    let sortedWorkItems_ByPosition = this.sortWorkItems(workItems);
-
-    // Obtiene el nombre de cada workItem
-    for(let workItem of sortedWorkItems_ByPosition) {
-      workItemsNames.push(workItem.name);
-    }
-
-    return workItemsNames;
-  }
-
-  // Ordena los workItems según el número de posición
-  sortWorkItems(workItems: WorkItem[]): WorkItem[] {
-    return workItems.sort(function(a, b) {
-      return a.position - b.position;
-    });
-  }
-
 }
