f4f532e3aa22dd244c1a401348d96407a4ceeddf
jmaus67 <javierms5@hotmail.com>
2022-06-07 20:53:34 +0200

    (PBI #61) Added: distinto comportamiento para el logotipo (enlace) tanto si el usuario ha iniciado sesión como si no

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
+
+// Models
+import { User } from 'src/app/models/user';
 
 // Services
 import { CookieService } from 'ngx-cookie-service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -12,8 +17,18 @@ import { CookieService } from 'ngx-cookie-service';
 export class NavbarComponent implements OnInit {
   title = 'Flow Estimates';
   uid = '';
+  user!: User;
 
-  constructor(private cookieService: CookieService, private router: Router) {}
+  constructor(private cookieService: CookieService, private router: Router, private userService: UserService) {}
+  
+  async ngOnInit() {
+    this.uid = this.cookieService.get('uid');
+  }
+
+  async getUser() {
+    if (!this.uid) return;
+    this.user = await lastValueFrom(this.userService.getUser(this.uid));
+  }
 
   async signOut() {
     this.cookieService.deleteAll();
@@ -21,10 +36,26 @@ export class NavbarComponent implements OnInit {
     if (this.cookieService.get('uid').length == 0) {
       this.uid = '';
     }
-    this.router.navigate(['/']);
+    await this.router.navigate(['/']);
   }
 
-  ngOnInit(): void {
-    this.uid = this.cookieService.get('uid');
+  async brandLink() {
+    // Si no hay usuario, volvemos a home
+    if (!this.uid) {
+      return this.router.navigate(['/']);
+    }
+
+    // Hay usuario
+    await this.getUser();
+    
+    // Si tiene openedProject
+    if (this.user.openedProject) {
+      // vamos al openedProject de user
+      return this.router.navigate([`/project/${this.user.openedProject}`]);
+    }
+
+    // Si no tiene openedProject, volvemos a home
+    return this.router.navigate(['/']);
   }
+
 }
